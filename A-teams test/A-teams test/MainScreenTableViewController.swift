63122af31fad2b8561abe6b02182e915ddfd2ce7
Batutist <Batutist@gmail.com>
2017-10-23 23:48:13 +0300

    add mainscreentableviewcontroller

---
+++ A-teams test/A-teams test/MainScreenTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainScreenTableViewController.swift
+//  A-teams test
+//
+//  Created by Ковалева on 23.10.17.
+//  Copyright © 2017 Ковалев. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainScreenTableViewController: UITableViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+}
