7571420559531c5462eea9151c4709a38e31282a
Batutist <Batutist@gmail.com>
2017-10-27 13:59:18 +0300

    create file to postsViewController

---
+++ A-teams test/A-teams test/PostsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PostsViewController.swift
+//  A-teams test
+//
+//  Created by Ковалева on 27.10.17.
+//  Copyright © 2017 Ковалев. All rights reserved.
+//
+
+import UIKit
+
+class PostsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
