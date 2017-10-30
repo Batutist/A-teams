a57da75a27e7a32dcbfe2ae61218a8d762eaa9a9
Batutist <Batutist@gmail.com>
2017-10-30 14:02:03 +0300

    Create file UsersViewController and linked to main.Storyboard

---
+++ A-teams test/A-teams test/UsersViewController.swift
@@ -0,0 +1,15 @@
+//
+//  UsersViewController.swift
+//  A-teams test
+//
+//  Created by Ковалева on 30.10.17.
+//  Copyright © 2017 Ковалев. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
