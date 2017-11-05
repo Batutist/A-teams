5e7b33b2034a85249b0bbb29a03c9bceac32f7ca
Batutist <Batutist@gmail.com>
2017-11-06 01:32:25 +0300

    Create file ToDosViewController. Connect it with viewController in Main.storyboard

---
+++ A-teams test/A-teams test/ToDosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ToDosViewController.swift
+//  A-teams test
+//
+//  Created by Ковалева on 06.11.17.
+//  Copyright © 2017 Ковалев. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ToDosViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
